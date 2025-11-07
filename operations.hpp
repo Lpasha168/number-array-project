673bc58dcd2ed436c461d18403e4e8c320f50ac4
Lpasha168 <lpasa6618@gmail.com>
2025-11-08 00:09:07 +0200

    Update operations.hpp

--- operations.hpp
+++ operations.hpp
@@ -1,9 +1,15 @@
-#pragmaonce
-#include "data.h"
+#pragma once 
+#include "data.hpp"
 
 namespace ArrayOperations {
     NumberArray* createArray(int size);
     void deleteArray(NumberArray* arr);
+    void applyOperation(NumberArray* arr, float (*operation)(float));
+    float square(float x);
+    float absolute(float x);
     void filterArray(NumberArray* arr, bool (*predicate)(float));
     float reduceArray(const NumberArray* arr, float (*operation)(float, float), float initial);
-}
+    bool isPositive(float x);
+    bool isEven(float x);
+    float add(float a, float b);
+    float multiply(float a, float b);
