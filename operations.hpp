89110ffaafb897336d4a9e97c980b8a4b79eed32
Lpasha168 <lpasa6618@gmail.com>
2025-11-07 23:34:24 +0200

    функции

--- operations.hpp
+++ operations.hpp
@@ -4,4 +4,6 @@
 namespace ArrayOperations {
     NumberArray* createArray(int size);
     void deleteArray(NumberArray* arr);
+    void filterArray(NumberArray* arr, bool (*predicate)(float));
+    float reduceArray(const NumberArray* arr, float (*operation)(float, float), float initial);
 }
